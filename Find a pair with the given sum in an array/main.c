28d3e39f5f78a2dda5428be39cf3529d38e1d693
Ahmed Osama <128548730+ahmed-osama10@users.noreply.github.com>
2023-04-26 11:44:13 +0200

    Update main.c

--- Find a pair with the given sum in an array/main.c
+++ Find a pair with the given sum in an array/main.c
@@ -1,17 +1,4 @@
-"""
-The function findPair() takes three arguments - an integer array nums, the size of the array n, and the target sum target. 
-The function starts by iterating over each element in the array except the last one using a loop that runs from i = 0 to n-1. 
-For each element at position i, another loop runs from j = i + 1 to n to compare the element at i with every other element after it.
 
-If the sum of the current element at i and the element at j is equal to the target sum,
-then a pair of integers is found and the function prints it to the console using the printf() function. 
-The return statement ensures that the function terminates after printing the first pair that is found.
-
-If the loop completes without finding a pair, then the function prints "Pair not found" to the console.
-
-This algorithm has a time complexity of O(n^2), which means that it will become very slow for large input arrays. 
-There are more efficient algorithms that can solve this problem in O(n) or O(nlogn) time complexity.
-"""
 #include <stdio.h>
 
 // Naive method to find a pair in an array with a given sum
@@ -47,3 +34,18 @@ int main(void)
 
 	return 0;
 }
+
+"""
+The function findPair() takes three arguments - an integer array nums, the size of the array n, and the target sum target. 
+The function starts by iterating over each element in the array except the last one using a loop that runs from i = 0 to n-1. 
+For each element at position i, another loop runs from j = i + 1 to n to compare the element at i with every other element after it.
+
+If the sum of the current element at i and the element at j is equal to the target sum,
+then a pair of integers is found and the function prints it to the console using the printf() function. 
+The return statement ensures that the function terminates after printing the first pair that is found.
+
+If the loop completes without finding a pair, then the function prints "Pair not found" to the console.
+
+This algorithm has a time complexity of O(n^2), which means that it will become very slow for large input arrays. 
+There are more efficient algorithms that can solve this problem in O(n) or O(nlogn) time complexity.
+"""
